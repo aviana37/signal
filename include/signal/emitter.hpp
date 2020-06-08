5fa68596df6dd01f365b88e02c7e87da66081bc5
André Viana <zanvictus@gmail.com>
2020-06-07 22:16:08 -0300

    Improved emitter/receiver disconnections.

--- include/signal/emitter.hpp
+++ include/signal/emitter.hpp
@@ -2,9 +2,9 @@
 #include "common.hpp"
 #include <forward_list>
 
-namespace signal 
+namespace signal
 {
-    namespace detail 
+    namespace detail
     {
         template <typename _signal>
         class emitter_instance
@@ -12,8 +12,8 @@ namespace signal
         public:
             emitter_instance() : m_receivers{} {}
             ~emitter_instance() {
-                for(auto r : m_receivers) {
-                    if(r) signal::disconnect(*r);
+                for (auto r : m_receivers) {
+                    if (r) signal::disconnect(*r);
                 }
             }
 
@@ -31,13 +31,13 @@ namespace signal
             void disconnect() {
                 m_receivers.clear();
             }
-            void disconnect(receiver_ptr receiver) {
-                m_receivers.remove(receiver);
+            bool disconnect(receiver_ptr receiver) {
+                return m_receivers.remove(receiver) > 0;
             }
 
             friend class emitter_access<_signal>;
         };
-        
+
         template <typename _signal>
         struct make_emitter_instance {
             using type = emitter_instance<_signal>;
@@ -52,8 +52,11 @@ namespace signal
             static void connect(emitter_instance<_signal>* e, receiver_instance<_signal>* r) {
                 e->connect(r);
             }
-            static void disconnect(emitter_instance<_signal>* e, receiver_instance<_signal>* r) {
-                e->disconnect(r);
+            static void disconnect(emitter_instance<_signal>* e) {
+                e->disconnect();
+            }
+            static bool disconnect(emitter_instance<_signal>* e, receiver_instance<_signal>* r) {
+                return e->disconnect(r);
             }
         };
     }
--- include/signal/signal.hpp
+++ include/signal/signal.hpp
@@ -3,7 +3,7 @@
 #include "emitter.hpp"
 #include "receiver.hpp"
 
-namespace signal 
+namespace signal
 {
     #define declare_signal(SIGNAL_NAME, ...) \
     struct __secret_tag_for_##SIGNAL_NAME {}; \
@@ -11,35 +11,38 @@ namespace signal
 
     template <typename _signal, typename F>
     requires detail::valid_signature<_signal, F>
-    void connect(detail::emitter_instance <_signal>& emitter, detail::receiver_instance<_signal>& receiver, F&& slot) {
-        if(receiver_access<_signal>::is_connected(&receiver))
+        void connect(detail::emitter_instance <_signal>& emitter, detail::receiver_instance<_signal>& receiver, F&& slot) {
+        if (receiver_access<_signal>::is_connected(&receiver))
             disconnect<_signal>(receiver);
-        
+
         detail::emitter_access <_signal>::connect(&emitter, &receiver);
-        detail::receiver_access<_signal>::connect(&emitter, &receiver, std::move(slot));   
+        detail::receiver_access<_signal>::connect(&emitter, &receiver, std::move(slot));
     }
 
-    template <typename _signal> void disconnect(detail::emitter_instance<_signal>& emitter) {
-        const auto& receivers = detail::emitter_access<_signal>::receivers();
-        for(auto r_ptr : receivers) { detail::receiver_access<_signal>::disconnect(r_ptr); }
+    template <typename _signal>
+    void disconnect(detail::emitter_instance<_signal>& emitter) {
+        const auto& receivers = detail::emitter_access<_signal>::receivers(&emitter);
+        for (auto r_ptr : receivers) { detail::receiver_access<_signal>::disconnect(r_ptr); }
         detail::emitter_access<_signal>::disconnect(&emitter);
     }
-    template <typename _signal> void disconnect(detail::receiver_instance<_signal>& receiver) {
+    template <typename _signal>
+    void disconnect(detail::receiver_instance<_signal>& receiver) {
         detail::emitter_access<_signal>::disconnect(detail::receiver_access<_signal>::emitter(&receiver), &receiver);
         detail::receiver_access<_signal>::disconnect(&receiver);
     }
-    template <typename _signal> 
+    template <typename _signal>
     void disconnect(detail::emitter_instance <_signal>& emitter, detail::receiver_instance<_signal>& receiver) {
-        detail::emitter_access<_signal>::disconnect(&emitter);
-        detail::receiver_access<_signal>::disconnect(&receiver);
+        //if emitter and receiver are connected, disconnect
+        if (detail::emitter_access<_signal>::disconnect(&emitter, &receiver))
+            detail::receiver_access<_signal>::disconnect(&receiver);
     }
 
     template <typename _signal>
     void emit(detail::emitter_instance<_signal>& emitter, typename _signal::tuple_type signal_data) {
         const auto& receivers = detail::emitter_access<_signal>::receivers(&emitter);
-        for(auto r_ptr : receivers) { detail::receiver_access<_signal>::push(r_ptr, signal_data); }
+        for (auto r_ptr : receivers) { detail::receiver_access<_signal>::push(r_ptr, signal_data); }
     }
-    
+
     template <typename _signal>
     void receive(detail::receiver_instance<_signal>& receiver) {
         detail::receiver_access<_signal>::receive(&receiver);
